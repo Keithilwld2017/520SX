272a878b4fea1cd5045f5d31584ee5f5aaa69012
KeithIlwld <bin.peng@dhkeji.cn>
2017-11-03 17:03:15 +0800

    add feature-2 on feature-2 branch

--- app/src/main/java/com/dh/sx520/MainActivity.java
+++ app/src/main/java/com/dh/sx520/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 public class MainActivity extends Activity {
 
     private Button btn_feature_1;
+    private Button btn_feature_2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +20,7 @@ public class MainActivity extends Activity {
 
     private void viewInit(){
         btn_feature_1 = findViewById(R.id.btn_feature1);
+        btn_feature_2 = findViewById(R.id.btn_feature2);
     }
 
 
@@ -34,4 +36,18 @@ public class MainActivity extends Activity {
             btn_feature_1.setText(R.string.feature_1);
         }
     }
+
+    /**
+     * feature-2
+     * display HelloWorld
+     * @param view view
+     */
+    public void feature2(View view){
+        if (btn_feature_2.getText().toString().equals("功能2")){
+            btn_feature_2.setText(R.string.feature_2_dis);
+        }else {
+            btn_feature_2.setText(R.string.feature_2);
+        }
+    }
+
 }
