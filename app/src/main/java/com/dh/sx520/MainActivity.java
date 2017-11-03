45e45843d1ea3070b80169f47f53b0fe9f966d32
KeithIlwld <bin.peng@dhkeji.cn>
2017-11-03 16:35:48 +0800

    add feature-1 on feature-1 branch

--- app/src/main/java/com/dh/sx520/MainActivity.java
+++ app/src/main/java/com/dh/sx520/MainActivity.java
@@ -1,13 +1,37 @@
 package com.dh.sx520;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private Button btn_feature_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewInit();
+    }
+
+
+    private void viewInit(){
+        btn_feature_1 = findViewById(R.id.btn_feature1);
+    }
+
+
+    /**
+     * feature-1
+     * display HelloWorld
+     * @param view view
+     */
+    public void feature1(View view){
+        if (btn_feature_1.getText().toString().equals("功能1")){
+            btn_feature_1.setText(R.string.feature_1_dis);
+        }else {
+            btn_feature_1.setText(R.string.feature_1);
+        }
     }
 }
